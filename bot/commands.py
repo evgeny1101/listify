77d6420d349f5eda4e170cd8e5827c1207ed58ed
Evgeniy1101 <evgenyostapenko.13@gmail.com>
2026-04-09 20:37:01 +0300

    Add usage example to /del command description

--- bot/commands.py
+++ bot/commands.py
@@ -3,6 +3,6 @@ from aiogram.types import BotCommand
 commands = [
     BotCommand(command="add", description="Добавить запись"),
     BotCommand(command="list", description="Посмотреть все записи"),
-    BotCommand(command="del", description="Удалить запись"),
+    BotCommand(command="del", description="Удалить запись <id>"),
     BotCommand(command="help", description="Помощь"),
 ]
